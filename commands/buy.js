465a2e248cea3290e20edeac457387fd9b7181de
Shayer Irtiza Khan <shayer687@gmail.com>
2021-03-14 11:05:54 +0600

    e

--- commands/buy.js
+++ commands/buy.js
@@ -27,6 +27,8 @@ module.exports = {
         
             });
             dayModel1.save();
+            console.log('The Day Database was created');
+            return
     
         } 
 
@@ -39,31 +41,18 @@ module.exports = {
 
             });
             itemData1.save();
-            return message.channel.send("Creating the Databases...")
+            message.channel.send("Creating the Databases...")
+            console.log('The Item Database was created');
+            return
         }
 
         let date = new Date();
-        let day = date.getDay();
-        
-        
-        let GreenLeaves = itemModel.GreenLeavesPrice;
+        let day = date.getHours();
         
 
         console.log(day);
 
-        if(day === 0){
-            await dayData.findOneAndUpdate(
-                {
-                    title: 'DayDB'
-
-                },
-                {                   
-                 Sunday: true,         
-                })
-
-            }
-
-        if(!GreenLeaves && dayModel.Sunday){
+        if(day === 10 && !dayModel.Sunday){
             const max = Math.ceil(120);
             const min = Math.ceil(90);
             let randomAmount = Math.floor(Math.random() * (max - min)) + min;
@@ -73,33 +62,218 @@ module.exports = {
                     title: "ItemDB"
                 },{
                     GreenLeavesPrice: randomAmount
-                });
+                })
+        
+            await dayData.findOneAndUpdate(
+                {
+                    title: 'DayDB'
+
+                },
+                {                   
+                    Sunday: true,         
+                    Monday: false,
+                    Tuesday: false,
+                    Wednesday: false,
+                    Thursday: false,
+                    Friday: false, 
+                    Saturday: false        
+                })
+              
+                console.log('The Price of GreenLeaves was randomized');
+
+
             }
 
-        if(!dayModel.Sunday){
-         await itemData.findOneAndUpdate(
-             {
-                 title: 'ItemDB'
-             },{
-                 GreenLeavesPrice: 0
-             });   
-         return
-        } 
+            if(day === 11 && !dayModel.Monday){
+                const max = Math.ceil(200);
+                const min = Math.ceil(0);
+                let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+    
+                await itemData.findOneAndUpdate(
+                    {
+                        title: "ItemDB"
+                    },{
+                        GreenLeavesPrice: randomAmount
+                    })
+            
+                await dayData.findOneAndUpdate(
+                    {
+                        title: 'DayDB'
+    
+                    },
+                    {                   
+                     Sunday: false,         
+                     Monday: true,
+                     Tuesday: false,
+                     Wednesday: false,
+                     Thursday: false,
+                     Friday: false, 
+                     Saturday: false
+                    })
+
+                    console.log('The Price of GreenLeaves was randomized');
+
+    
+                }
+
+                if(day === 12 && !dayModel.Tuesday){
+                    const max = Math.ceil(200);
+                    const min = Math.ceil(0);
+                    let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+        
+                    await itemData.findOneAndUpdate(
+                        {
+                            title: "ItemDB"
+                        },{
+                            GreenLeavesPrice: randomAmount
+                        })
+                
+                    await dayData.findOneAndUpdate(
+                        {
+                            title: 'DayDB'
+        
+                        },
+                        {                   
+                         Sunday: false,         
+                         Monday: false,
+                         Tuesday: true,
+                         Wednesday: false,
+                         Thursday: false,
+                         Friday: false, 
+                         Saturday: false
+                        })
+
+
+        
+                    }    
+
+                    if(day === 13 && !dayModel.Wednesday){
+                        const max = Math.ceil(300);
+                        const min = Math.ceil(50);
+                        let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+            
+                        await itemData.findOneAndUpdate(
+                            {
+                                title: "ItemDB"
+                            },{
+                                GreenLeavesPrice: randomAmount
+                            })
+                    
+                        await dayData.findOneAndUpdate(
+                            {
+                                title: 'DayDB'
+            
+                            },
+                            {                   
+                             Sunday: false,         
+                             Monday: false,
+                             Tuesday: false,
+                             Wednesday: true,
+                             Thursday: false,
+                             Friday: false, 
+                             Saturday: false
+                            })
+            
+                        }
+
+                        if(day === 14 && !dayModel.Thursday){
+                            const max = Math.ceil(250);
+                            const min = Math.ceil(0);
+                            let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+                
+                            await itemData.findOneAndUpdate(
+                                {
+                                    title: "ItemDB"
+                                },{
+                                    GreenLeavesPrice: randomAmount
+                                })
+                    
+                            await dayData.findOneAndUpdate(
+                                {
+                                    title: 'DayDB'
+                
+                                },
+                                {                   
+                                 Sunday: false,         
+                                 Monday: false,
+                                 Tuesday: false,
+                                 Wednesday: false,
+                                 Thursday: true,
+                                 Friday: false, 
+                                 Saturday: false
+                                })
+                
+                            }
+
+                            if(day === 15 && !dayModel.Friday){
+                                const max = Math.ceil(200);
+                                const min = Math.ceil(0);
+                                let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+                    
+                                await itemData.findOneAndUpdate(
+                                    {
+                                        title: "ItemDB"
+                                    },{
+                                        GreenLeavesPrice: randomAmount
+                                    })
+                            
+                                await dayData.findOneAndUpdate(
+                                    {
+                                        title: 'DayDB'
+                    
+                                    },
+                                    {                   
+                                     Sunday: false,         
+                                     Monday: false,
+                                     Tuesday: false,
+                                     Wednesday: false,
+                                     Thursday: false,
+                                     Friday: true, 
+                                     Saturday: false
+                                    })
+                    
+                                }
+
+                                if(day === 16 && !dayModel.Saturday){
+                                    const max = Math.ceil(400);
+                                    const min = Math.ceil(50);
+                                    let randomAmount = Math.floor(Math.random() * (max - min)) + min;
+                        
+                                    await itemData.findOneAndUpdate(
+                                        {
+                                            title: "ItemDB"
+                                        },{
+                                            GreenLeavesPrice: randomAmount
+                                        })
+                                
+                                    await dayData.findOneAndUpdate(
+                                        {
+                                            title: 'DayDB'
+                        
+                                        },
+                                        {                   
+                                         Sunday: false,         
+                                         Monday: false,
+                                         Tuesday: false,
+                                         Wednesday: false,
+                                         Thursday: false,
+                                         Friday: false, 
+                                         Saturday: true
+                                        })
+                        
+                                    }
+    
 
             if(cmd === 'check'){
 
-                if(!dayModel.Sunday) return message.channel.send('It is Sunday!');
-
-                if(GreenLeaves)
+                let priceEmbed = new Discord.MessageEmbed()
+                    .setColor('RANDOM')
+                    .setDescription(`The Price of Green Leaves today is ${itemModel.GreenLeavesPrice} Koins!`)                
             
-                    message.channel.send(`The Price of Green Leaves today is ${GreenLeaves}`);
+                     return message.channel.send(priceEmbed);
        
+            
             }
-            
-            
-    
-        
-
     }
 }
 
